543f718083a1707b60358aae0574a1206b37c024
Edd002 <42621512+Edd002@users.noreply.github.com>
2022-04-04 20:37:48 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/instagram_clone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.social_now_projeto_integrado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/global_variable.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:social_now_projeto_integrado/screens/add_post_screen.dart';
+import 'package:social_now_projeto_integrado/screens/feed_screen.dart';
+import 'package:social_now_projeto_integrado/screens/profile_screen.dart';
+import 'package:social_now_projeto_integrado/screens/search_screen.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
